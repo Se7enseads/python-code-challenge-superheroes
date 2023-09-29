8a6811c27b1565b25550db15917416cd37a69a0c
Kyle Mututo <135003824+Se7enseads@users.noreply.github.com>
2023-09-29 21:51:24 +0300

    feat: add a simple config & simple routes

--- app/app.py
+++ app/app.py
@@ -1,22 +1,47 @@
 #!/usr/bin/env python3
 
+from models import db, HeroPower
+import os
+
 from flask import Flask, make_response
 from flask_migrate import Migrate
+from flask_restful import Api, Resource
 
-from models import db, Hero
+from dotenv import load_dotenv
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/app.db'
+load_dotenv()
+
+
+app = Flask(
+    __name__,
+    static_url_path='',
+    static_folder='../client/build',
+    template_folder='../client/build'
+)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///app.db" # os.getenv('DATABASE_URI)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
 
 migrate = Migrate(app, db)
 
 db.init_app(app)
-
-@app.route('/')
-def home():
-    return ''
+api = Api(app)
 
 
-if __name__ == '__main__':
-    app.run(port=5555)
+class Home(Resource):
+    def get(self):
+        heroes = HeroPower.query.all()
+
+        heroes_data = []
+        for hero in heroes:
+            hero_data = {
+                'id': hero.id,
+                'strength': hero.strength
+            }
+            heroes_data.append(hero_data)
+
+        return heroes_data, 200
+
+
+api.add_resource(Home, '/')
